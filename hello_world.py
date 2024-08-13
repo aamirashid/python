a40e4a102af6b078134c90d254c26b739d2d4feb
Amir Rashid <66682973+aamirashid@users.noreply.github.com>
2024-08-13 18:58:24 +0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+print("Hello World!.")
+print('My favorite fruits are apple', 'banana', 'grapes', sep=' & ')
---
+++ variables.py
@@ -0,0 +1,18 @@
+# Python Data Types
+# Primitive
+# string: str
+# integer: int
+# float: float
+# Boolean: bool
+
+name:str            # Declaration 
+name = "amir   "    # Initialization
+age:int = 59        # Declaration & Initialization
+height:float = 5.7
+married_status:bool = True
+
+print('My name is', name.title(),'.')
+print('My name is', name.rstrip(),'.')
+print(f'I am {age} years old.')
+print('My height is ', height)
+print('This is\tPIAIC\nBatch-62 class')
